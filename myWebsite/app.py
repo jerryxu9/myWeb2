f9c7c5d63437d3551ed26117a4c3854f1b632790
Jerry Xu <jerryxwang9@gmail.com>
2019-12-03 23:37:31 -0800

    Jerry: 11:37pm, more features added

--- myWebsite/app.py
+++ myWebsite/app.py
@@ -8,5 +8,21 @@ app = flask.Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
